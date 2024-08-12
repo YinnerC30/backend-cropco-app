51b7ecef4b7ce366be16d097d75b428aaad25491
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2024-08-12 17:40:45 -0500

    funcionalidad de crear pdf con texto

--- src/printer/printer.module.ts
+++ src/printer/printer.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrinterService } from './printer.service';
 
+import { PrinterController } from './printer.controller';
+
 @Module({
   providers: [PrinterService],
   exports: [PrinterService],
+  controllers: [PrinterController],
 })
 export class PrinterModule {}
--- src/printer/printer.service.ts
+++ src/printer/printer.service.ts
@@ -1,11 +1,7 @@
-// https://fonts.google.com/selection
 import { Injectable } from '@nestjs/common';
 import PdfPrinter from 'pdfmake';
-import {
-  BufferOptions,
-  CustomTableLayout,
-  TDocumentDefinitions,
-} from 'pdfmake/interfaces';
+import { BufferOptions, TDocumentDefinitions } from 'pdfmake/interfaces';
+import { getHelloWorldReport } from './reports/hello-world.report';
 
 const fonts = {
   Roboto: {
@@ -16,57 +12,22 @@ const fonts = {
   },
 };
 
-const customTableLayouts: Record<string, CustomTableLayout> = {
-  customLayout01: {
-    hLineWidth: function (i, node) {
-      if (i === 0 || i === node.table.body.length) {
-        return 0;
-      }
-      return i === node.table.headerRows ? 2 : 1;
-    },
-    vLineWidth: function () {
-      return 0;
-    },
-    hLineColor: function (i) {
-      return i === 1 ? 'black' : '#bbbbbb';
-    },
-    paddingLeft: function (i) {
-      return i === 0 ? 0 : 8;
-    },
-    paddingRight: function (i, node) {
-      return i === node.table.widths.length - 1 ? 0 : 8;
-    },
-    fillColor: function (i, node) {
-      if (i === 0) {
-        return '#7b90be';
-      }
-      if (i === node.table.body.length - 1) {
-        return '#acb3c1';
-      }
-
-      return i % 2 === 0 ? '#f3f3f3' : null;
-    },
-  },
-  borderBlue: {
-    hLineColor: function () {
-      return '#5f96d4';
-    },
-    vLineColor: function () {
-      return '#5f96d4';
-    },
-  },
-};
-
 @Injectable()
 export class PrinterService {
   private printer = new PdfPrinter(fonts);
 
   createPdf(
     docDefinition: TDocumentDefinitions,
-    options: BufferOptions = {
-      tableLayouts: customTableLayouts,
-    },
+    options: BufferOptions = {},
   ): PDFKit.PDFDocument {
     return this.printer.createPdfKitDocument(docDefinition, options);
+  }
+
+  test() {
+    const docDefinition = getHelloWorldReport({
+      name: 'Fernando Herrera',
+    });
+    const doc = this.createPdf(docDefinition);
+    return doc;
   }
 }
