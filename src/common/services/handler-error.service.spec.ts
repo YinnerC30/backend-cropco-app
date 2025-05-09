69c76c65624f649d276b9d36ea5b0f34e3b66056
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-05-09 09:35:30 -0500

    fix: 🐛 test units for handlerError and exception harvest stock

--- src/common/services/handler-error.service.spec.ts
+++ src/common/services/handler-error.service.spec.ts
@@ -18,57 +18,58 @@ describe('HandlerErrorService', () => {
 
     service = module.get<HandlerErrorService>(HandlerErrorService);
     logger = new Logger();
-    service.setLogger(logger);
+    // service.setLogger(logger);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it('should throw an error if logger is not set', () => {
-    const errorService = new HandlerErrorService();
-    expect(() => errorService.handle(new Error('Test error'))).toThrow(
-      'Logger not set in HandlerErrorService',
-    );
-  });
-
   it('should log the error', () => {
     const loggerSpy = jest.spyOn(logger, 'error');
     const error = new Error('Test error');
-    expect(() => service.handle(error)).toThrow(InternalServerErrorException);
+    expect(() => service.handle(error, logger)).toThrow(
+      InternalServerErrorException,
+    );
     expect(loggerSpy).toHaveBeenCalledWith(error);
   });
 
   it('should throw BadRequestException for foreign key constraint violation', () => {
     const error = { code: '23503', detail: 'Foreign key violation' };
-    expect(() => service.handle(error)).toThrow(BadRequestException);
-    expect(() => service.handle(error)).toThrow(
+    expect(() => service.handle(error, logger)).toThrow(BadRequestException);
+    expect(() => service.handle(error, logger)).toThrow(
       'Foreign key constraint violation, Foreign key violation',
     );
   });
 
   it('should throw BadRequestException for unique constraint violation', () => {
     const error = { code: '23505', detail: 'Unique constraint violation' };
-    expect(() => service.handle(error)).toThrow(BadRequestException);
-    expect(() => service.handle(error)).toThrow(
+    expect(() => service.handle(error, logger)).toThrow(BadRequestException);
+    expect(() => service.handle(error, logger)).toThrow(
       'Unique constraint violation, Unique constraint violation',
     );
   });
 
   it('should rethrow HttpException', () => {
     const httpException = new BadRequestException('Http error');
-    expect(() => service.handle(httpException)).toThrow(httpException);
+    expect(() => service.handle(httpException, logger)).toThrow(httpException);
   });
 
   it('should throw BadRequestException for UpdateValuesMissingError', () => {
     const error = new UpdateValuesMissingError();
-    expect(() => service.handle(error)).toThrow(BadRequestException);
-    expect(() => service.handle(error)).toThrow('No values in the object');
+    expect(() => service.handle(error, logger)).toThrow(BadRequestException);
+    expect(() => service.handle(error, logger)).toThrow(
+      'No values in the object',
+    );
   });
 
   it('should throw InternalServerErrorException for unknown errors', () => {
     const error = new Error('Unknown error');
-    expect(() => service.handle(error)).toThrow(InternalServerErrorException);
-    expect(() => service.handle(error)).toThrow('An unexpected error occurred');
+    expect(() => service.handle(error, logger)).toThrow(
+      InternalServerErrorException,
+    );
+    expect(() => service.handle(error, logger)).toThrow(
+      'An unexpected error occurred',
+    );
   });
 });
