0f1b50af8efe3997a4164adf8f1f07a30d37dc90
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-06-10 17:53:05 -0500

    feat: 🚀 agregar soporte para HTTPS en la configuración del servidor y actualizar la URL de origen para producción

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,29 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
   const statusProject = process.env.STATUS_PROJECT || 'development';
   const hostFrontend = process.env.HOST_FRONTED;
   const portBackend = process.env.PORT_BACKEND;
+  
+  // Configuración HTTPS
+  const httpsOptions = {
+    key: fs.readFileSync(path.join(__dirname, '..', 'certs', 'private.key')),
+    cert: fs.readFileSync(path.join(__dirname, '..', 'certs', 'certificate.crt')),
+  };
+
   const app = await NestFactory.create(AppModule, {
     cors:
       statusProject === 'production'
         ? {
-            origin: !hostFrontend ? 'https://cropco.netlify.app' : hostFrontend,
+            origin: !hostFrontend ? 'https://cropco.org' : hostFrontend,
             credentials: true,
           }
         : true,
-    // cors: true
+    httpsOptions: statusProject === 'production' ? httpsOptions : undefined,
   });
 
   app.useGlobalPipes(
@@ -25,6 +34,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  
   console.log(
     'Application is running on port',
     portBackend,
