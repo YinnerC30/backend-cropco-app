6b3a88dcb83565ee5ef8badaddc34ad1a1da09d8
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-06-15 18:55:19 -0500

    feat: 🚀 mejorar la gestión de conexiones de tenants, añadiendo manejo de errores y optimizando la inicialización y cierre de conexiones

--- src/tenants/services/tenant-connection.service.ts
+++ src/tenants/services/tenant-connection.service.ts
@@ -1,51 +1,66 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { TenantsService } from '../tenants.service';
+import { HandlerErrorService } from 'src/common/services/handler-error.service';
 
 @Injectable()
 export class TenantConnectionService {
+  private readonly logger = new Logger('TenantsConnectionsService');
   private tenantConnections: Map<string, DataSource> = new Map();
 
   constructor(
     @Inject(forwardRef(() => TenantsService))
     private readonly tenantsService: TenantsService,
+    private readonly handlerError: HandlerErrorService,
   ) {}
 
   async getTenantConnection(tenantId: string): Promise<DataSource> {
-    if (!this.tenantConnections.has(tenantId)) {
-      const { config } =
-        await this.tenantsService.getOneTenantConfigDB(tenantId);
+    try {
+      if (!this.tenantConnections.has(tenantId)) {
+        const { config } =
+          await this.tenantsService.getOneTenantConfigDB(tenantId);
 
-      const dataSource = new DataSource({
-        type: 'postgres',
-        host: config.host,
-        port: config.port,
-        username: config.username,
-        password: config.password,
-        database: config.database,
-        entities: [__dirname + '/../../**/!(*tenant*).entity{.ts,.js}'],
-        synchronize: false,
-      });
+        const dataSource = new DataSource({
+          type: 'postgres',
+          host: config.host,
+          port: config.port,
+          username: config.username,
+          password: config.password,
+          database: config.database,
+          entities: [__dirname + '/../../**/!(*tenant*).entity{.ts,.js}'],
+          synchronize: false,
+        });
 
-      await dataSource.initialize();
+        await dataSource.initialize();
 
-      this.tenantConnections.set(tenantId, dataSource);
+        this.tenantConnections.set(tenantId, dataSource);
+      }
+
+      return this.tenantConnections.get(tenantId);
+    } catch (error) {
+      this.handlerError.handle(error, this.logger);
     }
-
-    return this.tenantConnections.get(tenantId);
   }
 
   async closeTenantConnection(tenantId: string): Promise<void> {
-    const connection = this.tenantConnections.get(tenantId);
-    if (connection) {
-      await connection.destroy();
-      this.tenantConnections.delete(tenantId);
+    try {
+      const connection = this.tenantConnections.get(tenantId);
+      if (connection) {
+        await connection.destroy();
+        this.tenantConnections.delete(tenantId);
+      }
+    } catch (error) {
+      this.handlerError.handle(error, this.logger);
     }
   }
 
   async closeAllConnections(): Promise<void> {
-    for (const [tenantId] of this.tenantConnections.entries()) {
-      await this.closeTenantConnection(tenantId);
+    try {
+      for (const [tenantId] of this.tenantConnections.entries()) {
+        await this.closeTenantConnection(tenantId);
+      }
+    } catch (error) {
+      this.handlerError.handle(error, this.logger);
     }
   }
 }
