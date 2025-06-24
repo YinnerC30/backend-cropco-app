a0181dea85ffd5c01a5fddfdeabb4ece29c2a1ad
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-06-24 16:02:18 -0500

    feat: 🚀 mejorar el interceptor de estado de respuesta para manejar múltiples estados, incluyendo conflictos y éxito, optimizando así la gestión de respuestas en la API.

--- src/common/interceptors/response-status.interceptor.ts
+++ src/common/interceptors/response-status.interceptor.ts
@@ -1,26 +1,28 @@
 import {
-    Injectable,
-    NestInterceptor,
-    ExecutionContext,
-    CallHandler,
-  } from '@nestjs/common';
-  import { Observable } from 'rxjs';
-  import { map } from 'rxjs/operators';
-  
-  @Injectable()
-  export class ResponseStatusInterceptor implements NestInterceptor {
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-      const response = context.switchToHttp().getResponse();
-  
-      return next.handle().pipe(
-        map((result) => {
-          if (result.failed && result.failed.length > 0) {
-            response.status(207); // Multi-Status
-          } else {
-            response.status(200); // OK
-          }
-          return result;
-        }),
-      );
-    }
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ResponseStatusInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const response = context.switchToHttp().getResponse();
+
+    return next.handle().pipe(
+      map((result) => {
+        if (result.failed.length > 0 && result.success.length > 0) {
+          response.status(207); // Multi-Status
+        } else if (result.failed.length > 0 && result.success.length === 0) {
+          response.status(409); // Conflict
+        } else if (result.success.length > 0 && result.failed.length === 0) {
+          response.status(200); // OK
+        }
+        return result;
+      }),
+    );
   }
+}
