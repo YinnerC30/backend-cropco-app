88f4a8173a2a6dbce6b8e80409ad928e8e7cdf0c
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-06-25 12:38:49 -0500

    feat: 🚀 agregar decorador MatchAmount en SaleDto para validar que el monto coincida con la suma de cantidades en los detalles de la venta, mejorando la integridad de los datos.

--- src/sales/dto/sale.dto.ts
+++ src/sales/dto/sale.dto.ts
@@ -9,6 +9,8 @@ import {
   ValidateNested,
 } from 'class-validator';
 import { SaleDetailsDto } from './sale-details.dto';
+import { MatchAmount } from 'src/common/decorators/match-amount.decorator';
+import { MatchTotals } from 'src/common/decorators/match-totals.decorator';
 
 export class SaleDto {
   @IsDateString()
@@ -16,10 +18,18 @@ export class SaleDto {
 
   @IsNumber()
   @IsPositive()
+  @MatchAmount({
+    nameArrayToCalculate: 'details',
+    targetUnit: 'GRAMOS',
+  })
   amount: number;
 
   @IsNumber()
   @IsPositive()
+  @MatchTotals({
+    fields: ['value_pay'],
+    nameArrayToCalculate: 'details',
+  })
   value_pay: number;
 
   @IsArray()
