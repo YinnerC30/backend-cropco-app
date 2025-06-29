f23dce464dff892c05779bbaf0b6144d1bdc9f71
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-06-29 11:13:01 -0500

    feat: 🚀 agregar el servicio de encriptación para mejorar la generación y manejo de contraseñas en el módulo común, optimizando la seguridad y la reutilización del código en los servicios de inquilinos.

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonalInformation } from './entities/personal-information.entity';
 import { HandlerErrorService } from './services/handler-error.service';
+import { EncryptionService } from './services/encryption.service';
 import { CookiesLoggerInterceptor } from './interceptors/cookies-logger.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PersonalInformation])],
-  providers: [HandlerErrorService, CookiesLoggerInterceptor],
-  exports: [TypeOrmModule, HandlerErrorService, CookiesLoggerInterceptor],
+  providers: [HandlerErrorService, EncryptionService, CookiesLoggerInterceptor],
+  exports: [TypeOrmModule, HandlerErrorService, EncryptionService, CookiesLoggerInterceptor],
 })
 export class CommonModule {}
