596545cb81b617a6d0b1c8c4fdecc8b73a6f607b
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-07-03 09:18:24 -0500

    feat: 🚀 actualizar configuración de entorno y agregar script para crear administrador. Se modifica el archivo .env.test para incluir nuevas credenciales de base de datos y se añade un script SQL para insertar un administrador por defecto en la base de datos.

---
+++ config/create-administrator.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.administrators (first_name,
+                                   last_name,
+                                   email,
+                                   cell_phone_number,
+                                   password,
+                                   role,
+                                   is_active)
+VALUES ('Administrator',
+        'CropCo',
+        'admincropco@mail.com',
+        '3243347549',
+        '$2b$10$Ko.8QGXNmo7eUP6z4CyZYObxLrau1B7m3uZshGNSe9bshyinUXigC',
+        'admin',
+        true);
