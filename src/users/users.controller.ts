a84b86758ba1dc22e8fd1f3535a2e2cc5d3ff2a4
Yinner Chilito <92528747+YinnerC30@users.noreply.github.com>
2025-07-18 17:03:16 -0500

    feat: 🚀 ajustar la limitación de tasa en el método de inicio de sesión a 10 solicitudes, mejorando la capacidad de respuesta del sistema. También se agrega un interceptor de estado de respuesta en la eliminación masiva de usuarios para una mejor gestión de respuestas.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Put,
   Post,
   Query,
+  UseInterceptors,
 } from '@nestjs/common';
 
 import { Auth } from 'src/auth/decorators/auth.decorator';
@@ -20,6 +21,7 @@ import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { GetPropertyFromToken } from 'src/auth/decorators/get-property-from-user-token.decorator';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ResponseStatusInterceptor } from 'src/common/interceptors/response-status.interceptor';
 
 export const pathsUsersController: PathsController = {
   createUser: {
@@ -115,6 +117,7 @@ export class UsersController {
   }
 
   @Delete(removeUsers.path)
+  @UseInterceptors(ResponseStatusInterceptor)
   removeBulk(@Body() removeBulkUsersDto: RemoveBulkRecordsDto<User>) {
     return this.usersService.removeBulk(removeBulkUsersDto);
   }
